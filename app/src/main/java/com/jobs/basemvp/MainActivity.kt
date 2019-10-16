17e89bc7e1e041610b598b4928472982b6c0653b
Khiem Nguyen <nguyenkhiemit@gmail.com>
2019-10-16 15:03:19 +0700

    create project

---
+++ app/src/main/java/com/jobs/basemvp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jobs.basemvp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

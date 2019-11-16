2f1048f06f69396e97473acdb51d6c938f3e8caa
Khiem Nguyen <nguyenkhiemit@gmail.com>
2019-11-16 14:37:05 +0700

    add dragger

---
+++ app/src/main/java/com/jobs/basemvp/di/module/AppModule.kt
@@ -0,0 +1,2 @@
+package com.jobs.basemvp.di.module
+
--- app/src/main/java/com/jobs/basemvp/ui/login/LoginPresenter.kt
+++ app/src/main/java/com/jobs/basemvp/ui/login/LoginPresenter.kt
@@ -1,5 +1,6 @@
 package com.jobs.basemvp.ui.login
 
+import android.util.Log
 import com.jobs.basemvp.BaseApplication.Companion.apiClient
 import com.jobs.basemvp.data.model.requests.LoginRequest
 import com.jobs.basemvp.ui.base.BasePresenter
@@ -21,11 +22,14 @@ class LoginPresenter: BasePresenter<LoginView>() {
                     .subscribe({
                         getView()?.hideLoading()
                         if(it.status != null && it.status == 200) {
+                            Log.e("Xlogin ", "===> 1")
                             getView()?.loginSuccess(it.data)
                         } else {
+                            Log.e("Xlogin ", "===> 2")
                             getView()?.loginFailure(it.message)
                         }
                     }, {
+                        Log.e("Xlogin ", "===> 3")
                         getView()?.hideLoading()
                         getView()?.loginFailure(it.message)
                     })
